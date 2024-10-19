532b9ea16df212367cfddc2599f1a3743fa8083b
david.cardenas <david.cardenas0718@gmail.com>
2024-10-19 12:27:02 -0500

    se agregan 4 codigos nuevos, fiuras geometricas (base), circulo, triangulo y rectangulo, todo basado desde la funcion principal el cual es el menu principal

---
+++ src/main/java/com/mycompany/figurasgeometricas/FiguraGeometrica.java
@@ -0,0 +1,39 @@
+
+package com.mycompany.figurasgeometricas;
+
+/**
+ *
+ * @author Usuario
+ */
+    public abstract class FiguraGeometrica {
+    private String nombre;
+    private String color;
+
+    public FiguraGeometrica(String nombre, String color) {
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public abstract double obtenerArea();
+    public abstract double obtenerPerimetro();
+}
+
+    
+    
+
